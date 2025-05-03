9bb1474ee252ecc3aa2af27df88614a27141b30c
Joodmohammed <joodmohammed998@gmail.com>
2025-05-03 16:30:24 +0300

    initial code

---
+++ Sorting.java
@@ -0,0 +1,110 @@
+
+import java.util.Arrays;
+
+public class Sorting  {
+   
+
+    public void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+
+    public void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public void merge(int[] arr, int left, int mid, int right) {
+        int[] leftArray = Arrays.copyOfRange(arr, left, mid + 1);
+        int[] rightArray = Arrays.copyOfRange(arr, mid + 1, right + 1);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] < rightArray[j]) {
+                arr[k++] = leftArray[i++];
+            } else {
+                arr[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < leftArray.length) {
+            arr[k++] = leftArray[i++];
+        }
+
+        while (j < rightArray.length) {
+            arr[k++] = rightArray[j++];
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        
+        Sorting sorting = new Sorting();
+       
+        
+        int[] arr = {64, 25, 12, 22, 11};
+        sorting.selectionSort(arr);
+        System.out.println("Selection Sort: " + Arrays.toString(arr));
+        
+   
+        arr = new int[]{64, 25, 12, 22, 11};
+        sorting.insertionSort(arr);
+        System.out.println("Insertion Sort: " + Arrays.toString(arr));
+
+     
+        arr = new int[]{64, 25, 12, 22, 11};
+        sorting.bubbleSort(arr);
+        System.out.println("Bubble Sort: " + Arrays.toString(arr));
+
+       
+        arr = new int[]{64, 25, 12, 22, 11};
+        sorting.mergeSort(arr, 0, arr.length - 1);
+        System.out.println("Merge Sort: " + Arrays.toString(arr));
+    }
+}
+   
+
+
+
