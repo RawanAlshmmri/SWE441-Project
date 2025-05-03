eb965a7ac794a0cfef35309a68ca279817ddf688
RawanAlshmmri <rwan9ix9@gmail.com>
2025-05-03 23:55:22 +0300

    SCRUM-7: Refactored QuickSort implementation

--- Sorting.java
+++ Sorting.java
@@ -82,31 +82,40 @@ public class Sorting  {
         System.arraycopy(temp, 0, arr, left, temp.length);
     }
 
+  
+public class QuickSortUtil {
+
     public void quickSort(int[] arr, int low, int high) {
         if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
         }
     }
 
-    public int partition(int[] arr, int low, int high) {
+    private int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        int i = (low - 1);
+        int i = low - 1;
+
         for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {
                 i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                swap(arr, i, j);
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
 
+        swap(arr, i + 1, high);
         return i + 1;
     }
+
+    private void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
 
     
     public static void main(String[] args) {
