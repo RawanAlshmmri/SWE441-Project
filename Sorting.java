be02b4b4824b613869de7fbce6e5ea55e8718d75
Joodmohammed <joodmohammed998@gmail.com>
2025-05-03 22:24:29 +0300

    SCRUM-6: Add new feature: Quick Sort

--- Sorting.java
+++ Sorting.java
@@ -82,6 +82,32 @@ public class Sorting  {
         System.arraycopy(temp, 0, arr, left, temp.length);
     }
 
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
     
     public static void main(String[] args) {
         
@@ -106,6 +132,10 @@ public class Sorting  {
         arr = new int[]{64, 25, 12, 22, 11};
         sorting.mergeSort(arr, 0, arr.length - 1);
         System.out.println("Merge Sort: " + Arrays.toString(arr));
+
+        arr = new int[]{64, 25, 12, 22, 11};
+        sorting.quickSort(arr, 0, arr.length - 1); 
+        System.out.println("Quick Sort: " + Arrays.toString(arr));
     }
 }
    
