d089af59cc8e7c21b77806b37c1bf4b77e4b2f9d
Reem-Alr <rreemm2002m@gmail.com>
2025-05-04 16:04:18 +0300

    SCRUM-12: Add new feature Heap sort

--- Sorting.java
+++ Sorting.java
@@ -158,7 +158,37 @@ public void reverseSort(int[] arr) {
         arr[arr.length - 1 - i] = temp;
     }
 }
+public void heapSort(int[] arr) {
+    int n = arr.length;
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
+    }
+}
 
+private void heapify(int[] arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int swap = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = swap;
+        heapify(arr, n, largest);
+    }
+}
 
 
     
@@ -192,6 +222,12 @@ public static void main(String[] args) {
     int[] arrForQuick = Arrays.copyOf(arr, arr.length);
     sorting.quickSort(arrForQuick, 0, arrForQuick.length - 1);
     System.out.println("Quick Sort: " + Arrays.toString(arrForQuick));
+
+
+    // Call heapsort
+    int[] arrForHeap = Arrays.copyOf(arr, arr.length);
+sorting.heapSort(arrForHeap);
+System.out.println("Heap Sort: " + Arrays.toString(arrForHeap));
 }
 }
    
