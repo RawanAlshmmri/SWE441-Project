2cfa34017a31b4482aed3f5923aa544cf4770ece
Reem-Alr <rreemm2002m@gmail.com>
2025-05-04 16:12:35 +0300

    SCRUM-13: Refactoring countingSort

--- Sorting.java
+++ Sorting.java
@@ -126,30 +126,25 @@ public class Sorting  {
     }
 
 
-public void countingSort(int[] arr) {
-    if (arr.length == 0)
-        return;
-
-    int max = arr[0];
-    for (int i = 1; i < arr.length; i++) {
-        if (arr[i] > max)
-            max = arr[i];
-    }
-
-    int[] count = new int[max + 1];
-
-    for (int i = 0; i < arr.length; i++) {
-        count[arr[i]]++;
-    }
-
-    int index = 0;
-    for (int i = 0; i < count.length; i++) {
-        while (count[i] > 0) {
-            arr[index++] = i;
-            count[i]--;
+    public void countingSort(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+    
+        int max = Arrays.stream(arr).max().getAsInt();
+    
+        int[] count = new int[max + 1];
+    
+        for (int num : arr) {
+            count[num]++;
+        }
+    
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            Arrays.fill(arr, index, index + count[i], i);
+            index += count[i];
         }
     }
-}
+    
 public void reverseSort(int[] arr) {
     Arrays.sort(arr); 
     for (int i = 0; i < arr.length / 2; i++) {
